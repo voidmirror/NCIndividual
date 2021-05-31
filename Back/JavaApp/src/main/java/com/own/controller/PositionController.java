ec58ebb3386f5f346e57f2583c62d993183c842e
voidmirror <49756045+voidmirror@users.noreply.github.com>
2021-05-31 17:14:30 +0300

    Angular blanc project added

--- Back/JavaApp/src/main/java/com/own/controller/PositionController.java
+++ Back/JavaApp/src/main/java/com/own/controller/PositionController.java
@@ -1,9 +1,12 @@
 package com.own.controller;
 
 import com.own.entity.Position;
+import com.own.exception.ResourceNotFoundException;
 import com.own.repository.PositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,15 @@ public class PositionController {
     @GetMapping("/positions")
     public List<Position> retrieveAllPositions() {
         return positionRepository.findAll();
+    }
+
+    @GetMapping("/positions/{id}")
+    public ResponseEntity<Position> findPositionById(
+            @PathVariable(value = "id") Integer positionId
+    ) throws ResourceNotFoundException {
+        Position position = positionRepository.findById(positionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Position not found for id: " + positionId));
+        return ResponseEntity.ok().body(position);
     }
 
 }
